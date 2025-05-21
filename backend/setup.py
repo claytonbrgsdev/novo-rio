843446cfc2fbba3da5b3bc5d172fc97dab9f3607
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2025-05-20 23:30:25 -0300

    fix: Atualiza setup.py para incluir extras_require com dependências de desenvolvimento

--- backend/setup.py
+++ backend/setup.py
@@ -5,4 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
+    extras_require={
+        'dev': open('requirements-dev.txt').read().splitlines(),
+    },
 )
